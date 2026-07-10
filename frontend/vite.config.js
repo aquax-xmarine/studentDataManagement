21e680074e07c40013200ff18a0652a715b593f1
Nikisha <snikisha002@gmail.com>
2026-07-10 21:55:52 +0545

    Added PWA back

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,34 +6,34 @@ export default defineConfig({
   plugins: [
     react(),
 
-    // VitePWA({
-    //   registerType: 'autoUpdate',
+    VitePWA({
+      registerType: 'autoUpdate',
 
-    //   manifest: {
-    //     name: 'Student Management System',
-    //     short_name: 'SMS',
-    //     description: 'Student Management Progressive Web App',
+      manifest: {
+        name: 'Student Management System',
+        short_name: 'SMS',
+        description: 'Student Management Progressive Web App',
 
-    //     theme_color: '#2563eb',
-    //     background_color: '#ffffff',
+        theme_color: '#2563eb',
+        background_color: '#ffffff',
 
-    //     display: 'standalone',
-    //     start_url: '/',
+        display: 'standalone',
+        start_url: '/',
 
-    //     icons: [
-    //       {
-    //         src: 'icon-192.png',
-    //         sizes: '192x192',
-    //         type: 'image/png'
-    //       },
-    //       {
-    //         src: 'icon-512.png',
-    //         sizes: '512x512',
-    //         type: 'image/png'
-    //       }
-    //     ]
-    //   }
-    // })
+        icons: [
+          {
+            src: 'icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
   ],
   server: {
     host: true,

9076c4909d4d9d9872293a90e3327bd5c1d9ca7f
Nikisha <snikisha002@gmail.com>
2026-07-21 06:53:19 +0545

    Database deployed

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,23 @@
+// const { Pool } = require("pg");
+
+// const pool = new Pool({
+//     user: "postgres",
+//     host: "student-db",
+//     database: "studentdb",
+//     password: "24119",
+//     port: 5432
+// });
+
+// module.exports = pool;
+
+
 const { Pool } = require("pg");
 
 const pool = new Pool({
-    user: "postgres",
-    host: "student-db",
-    database: "studentdb",
-    password: "24119",
-    port: 5432
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
